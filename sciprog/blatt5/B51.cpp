a289f278dcf995582e67c599074d87fbe656e973
Grompelfix <Grompelfix@users.noreply.github.com>
2016-06-08 12:53:46 +0200

    Update B51.cpp

--- sciprog/blatt5/B51.cpp
+++ sciprog/blatt5/B51.cpp
@@ -1,3 +1,20 @@
+**
+ * @author Birte Pajunk, Jan Schnitker, Yule Meyer-Olbersleben
+ * @file B51.cpp
+ * @desc Homework Blatt5.1 for SciProg
+ *
+ * wenn eine Exception geworfen wird, werden alle Destructoren aufgerufen
+ * -> Freigabe der Ressourcen sollte in den Destructoren geregelt sein und
+ * somit sollte der Speicher beim Wurf einer Exception wieder freigegeben werden.
+ * Vermutung warum das unter 'Note' beschriebene Verhalten sinnvoll ist:
+ * Wenn eine Exception in einem Destructor aufgerufen wird, werden alle
+ * Destructoren aufgerufen - auch nochmal der aus dem die Exception kam,
+ * bzw. wenn eine Exception geworfen und in Folge dessen ein Destructor
+ * aufgerufen wird, in dem eine weitere Exception geworfen wird, kommt es zu
+ * einem unvorhersehbaren Verhalten, da auf den Destructor eines bereits
+ * gelöschten Objekts zugegriffen wird.
+
+ */
 #include <iostream>
 #include <string>
 #include <exception>
