ccb6011f49ea47a51c89e0bf65b2f28668da823d
Grompelfix <Grompelfix@users.noreply.github.com>
2016-06-15 11:33:02 +0200

    Update Trapez.cpp

--- sciprog/blatt6/Trapez.cpp
+++ sciprog/blatt6/Trapez.cpp
@@ -1,3 +1,9 @@
+/**
+ * @author Birte Pajunk, Jan Schnitker, Yule Meyer-Olbersleben
+ * @file Trapez.cpp
+ * @desc Homework Blatt6 for SciProg
+ */
+
 #include "Trapez.h"
 
 double Trapez::computeQuadratur(Funktion* f, double a, double b)
